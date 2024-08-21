a90b63a32632d6ca842c74c07df7e1ff3a1a4a45
tuukka <tuukkauu@metropolia.fi>
2024-08-21 09:17:36 +0300

    Lisätty tehtävä 2.4

---
+++ tehtävä2.4.py
@@ -0,0 +1,10 @@
+import math
+
+kokonaisluku1 = input("Syötä kokonaisluku")
+kokonaisluku2 = input("Syötä kokonaisluku")
+kokonaisluku3 = input("Syötä kokonaisluku")
+
+print("lukujen summa: ",float(kokonaisluku1) + float(kokonaisluku2) + float(kokonaisluku3))
+print("lukujen tulo: ",float(kokonaisluku1)*float(kokonaisluku2)*float(kokonaisluku3))
+print("lukujen keskiarvo: ",(float(kokonaisluku1)+float(kokonaisluku2)+float(kokonaisluku3))/3)
+
