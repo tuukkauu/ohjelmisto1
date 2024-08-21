63692e32d0ab3f725c84cc00d459c83939999952
tuukka <tuukkauu@metropolia.fi>
2024-08-21 14:48:57 +0300

    Lisätty tehtävä 2.6

---
+++ tehtävä2.6.py
@@ -0,0 +1,12 @@
+import random
+
+num1 = random.randint(0,9)
+num2 = random.randint(0,9)
+num3 = random.randint(0,9)
+num4 = random.randint(1,6)
+num5 = random.randint(1,6)
+num6 = random.randint(1,6)
+num7 = random.randint(1,6)
+print(f"{num1}{num2}{num3}")
+print(num4,num5,num6,num7)
+
