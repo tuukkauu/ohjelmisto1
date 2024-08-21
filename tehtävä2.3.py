17e5d6195036c3fde43f2d95c6a6fa2af3aa05a0
tuukka <tuukkauu@metropolia.fi>
2024-08-21 08:55:43 +0300

    Lisätty tehtävä 2.1

---
+++ tehtävä2.3.py
@@ -0,0 +1,6 @@
+import math
+
+mitta1 = input("Syötä suorakulmion kanta")
+mitta2 = input("Syötä suorakulmion korkeus")
+
+print("Suorakulmion piiri: ",float(mitta1)*2 + float(mitta2)*2, "Suorakulmion pinta-ala: ",float(mitta1)*float(mitta2))
