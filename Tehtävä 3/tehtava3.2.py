ec02d653f0d0856276d622e3a1e982be92a4af83
tuukka <tuukkauu@metropolia.fi>
2024-08-23 10:54:52 +0300

    Lisätty tehtävä 3.2

---
+++ Tehtävä 3/tehtava3.2.py
@@ -0,0 +1,13 @@
+hyttiluokka = input("Syötä joki hyttiluokista LUX, A, B tai C: ")
+
+if hyttiluokka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif hyttiluokka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hyttiluokka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif hyttiluokka == "C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+
+else:
+    print("Virheellinen hyttiluokka.")
