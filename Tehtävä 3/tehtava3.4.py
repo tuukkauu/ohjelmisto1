4463ddba26698f8bf238ea1b2c03eaa23ffbb7fb
tuukka <tuukkauu@metropolia.fi>
2024-08-23 14:51:22 +0300

    Lisätty tehtävä 3.3

---
+++ Tehtävä 3/tehtava3.4.py
@@ -0,0 +1,10 @@
+import math
+
+vuosi = int(input("Kirjoita vuosiluku: "))
+
+if vuosi % 4 == 0 and vuosi % 100 != 0:
+    print("Antamasi vuosi on karkausvuosi.")
+elif vuosi % 100 == 0 and vuosi % 400 == 0 and vuosi % 4 == 0:
+    print("Antamasi vuosi on karkausvuosi.")
+else:
+    print("Antamasi vuosi ei ole karkausvuosi.")
