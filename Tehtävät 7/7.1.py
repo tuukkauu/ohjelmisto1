01e4756ae878b9c5974490915c950da951f82504
tuukka <tuukkauu@metropolia.fi>
2024-09-10 10:44:12 +0300

    Lisätty tehtävä 7.1

--- Tehtävät 7/7.1.py
+++ Tehtävät 7/7.1.py
@@ -13,11 +13,3 @@ if syote >= 9 and syote <= 11:
 
 if syote >= 1 and syote <= 2 or syote == 12:
     print(f"{syote}. kuukausi kuuluu {vuodenajat[0]}-kuukausiin.")
-
-
-"""
-talvi = 12, 1, 2
-kevat = 3, 4, 5
-kesa = 6, 7, 8
-syksy = 9, 10, 11
-"""
